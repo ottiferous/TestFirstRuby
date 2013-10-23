2d492dbc1af79472215d8e93d8b7423cf5b052dc
Andrew <github@andrewmarrone.com>
2013-10-23 15:51:28 -0700

    hardcoded words to capitalize fixed accessor
    found nifty %w{} notation for ruby and typecasting using [] over an
    argument to handle broken string easier

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,11 +1,18 @@
-class Book
-   def title=(string)
-      string.split.map(&:capitalize).join(' ')
-      @title = string
-   end
+class Book 
+   attr_reader :title
 
-   def title
-      @title
+   def title=(string) 
+#      @title = string.split.map(&:capitalize).join(' ')
+      result = string.split
+      result = [result[0].capitalize] +
+         result[1..-1].map do |word|
+            if %w{the a an and in of}.include? word
+               word
+            else
+               word.capitalize
+            end
+         end
+      @title = result.join(' ')
    end
 
 end
