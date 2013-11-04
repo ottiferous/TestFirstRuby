8dcbbe4f5167ae18e5d57cb2e9f21064a21f8fdd
Andrew <github@andrewmarrone.com>
2013-11-04 15:51:05 -0800

    created skeleton for Temperature Class

--- 10_temperature_object/temperature.rb
+++ 10_temperature_object/temperature.rb
@@ -1,3 +1,39 @@
 class Temperature
    
+   def initialize
+      
+   end
+   
+   def in_fahrenheit
+      
+   end
+   
+   def in_celsius
+      
+   end
+   
+   def from_celsius
+      
+   end
+   
+   def from_fahrenheit
+   
+   end
+   
+   def ftoc(deg)
+    (deg-32)*(5.0/9.0)
+   end
+
+   def ctof(deg)
+    deg * (9.0/5.0) + 32
+   end
+   
+end
+
+class Celsius < Temperature
+   
+end
+
+class Fahrenheit < Temperature
+   
 end
