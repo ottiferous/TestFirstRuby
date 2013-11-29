1ff30da0a6448ed09e9ac20da0c8ebc2c1afd61f
Andrew <github@andrewmarrone.com>
2013-11-28 18:20:35 -0800

    RPN Calculator Built
    all hail the stack!

---
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -0,0 +1,58 @@
+class RPNCalculator
+   
+   def initialize
+      @num_stack = []
+      @total = 0
+   end
+   
+   def push num
+      @num_stack.push num
+   end
+   
+   def tokens string
+      string.split.map! { |token| token[/\d/] ? token.to_i : token.to_sym }
+   end
+   
+   def evaluate string
+      tokens(string).each do |key|
+         case key
+            when Fixnum 
+               push key
+            when :+
+               plus
+            when :-
+               minus
+            when :*
+               times
+            when :/
+               divide
+         end
+      end
+      value
+   end
+   
+   def plus
+      raise "calculator is empty" unless @num_stack.size >= 2
+      @num_stack.push @num_stack.pop(2).inject(:+) 
+   end
+   
+   def minus
+      raise "calculator is empty" unless @num_stack.size >= 2
+      @num_stack.push @num_stack.pop(2).inject(:-)
+   end
+   
+   def times
+      raise "calculator is empty" unless @num_stack.size >= 2
+      @num_stack.push @num_stack.pop(2).inject(:*)
+   end
+   
+   def divide
+      raise "calculator is empty" unless @num_stack.size >= 2
+      @num_stack.push @num_stack.pop(2).inject(:fdiv)
+   end
+   
+   def value
+      @num_stack[-1]
+   end
+   
+end
