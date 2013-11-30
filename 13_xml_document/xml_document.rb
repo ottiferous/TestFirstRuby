3a6910c77358edc3522ee94688d6634483ec7af1
Andrew <github@andrewmarrone.com>
2013-11-29 22:32:02 -0800

    added indent method
    builder methods are fun!

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,18 +1,31 @@
 class XmlDocument
    
-   def method_missing(name, *args, &block)
+   def initialize indent = false
+      @indent = indent
+      @indent_lvl = 0
+   end
+   
+   def method_missing name, *args, &block
       attr = args[0] || {}
-      xml = "<#{name}"
+      xml = ""
+      xml += "  "*@indent_lvl if @indent
+      xml += "<#{name}"
       attr.each_pair do |key, value|
          xml += " #{key}='#{value}'"
       end
       
       if block
          xml += ">"
+         xml += "\n" if @indent
+         @indent_lvl += 1
          xml += yield
+         @indent_lvl -= 1
+         xml += "  "*@indent_lvl if @indent
          xml += "</#{name}>"
+         xml += "\n" if @indent
       else
          xml += "/>"
+         xml += "\n" if @indent
       end
       xml
    end
