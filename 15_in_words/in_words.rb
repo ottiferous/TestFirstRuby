d8883c6684693514ad1d6426e76b181c2bf4bba1
Andrew <github@andrewmarrone.com>
2013-12-02 16:52:40 -0800

    nothing special - saving work

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -1,8 +1,111 @@
 class Fixnum
    
+   def single num
+
+        case num
+        when 1
+           'one '
+        when 2
+           'two '
+        when 3
+           'three '
+        when 4
+           'four '
+        when 5
+           'five '
+        when 6
+           'six '
+        when 7
+           'seven '
+        when 8
+           'eight '
+        when 9
+           'nine '
+        end
+   end
+   
+   def ugly_switch num
+         
+      case num
+      when 1
+         'one '
+      when 2
+         'two '
+      when 3
+         'three '
+      when 4
+         'four '
+      when 5
+         'five '
+      when 6
+         'six '
+      when 7
+         'seven '
+      when 8
+         'eight '
+      when 9
+         'nine '
+      end
+
+      case num
+      when 10
+         'ten '
+      when 11
+         'eleven '
+      when 12
+         'twelve '
+      when 13
+         'thirteen '
+      when 14
+         'fourteen '
+      when 15
+         'fifteen '
+      when 16
+         'sixteen '
+      when 17
+         'seventeen '
+      when 18
+         'eighteen '
+      when 19
+         'nineteen '
+      when 20
+         'twenty '
+      when 30
+         'thirty '
+      when 40
+         'forty '
+      when 50
+         'fifty '
+      when 60
+         'sixty '
+      when 70
+         'seventy '
+      when 80
+         'eighty '
+      when 90
+         'ninety '
+      when 100
+         'one hundred '
+      else
+         ''
+      end
+   end
+   
    def in_words
-      # begin by reducing the number down into 'word units' and then we can identify
-      # each unit by comparing the values to hard-coded values
+      # odd_words are the ones that need to be evaluated as 'sixty', 'seventy' etc.
+      
+      return 'zero' if self == 0    # special case, and breaks the pattern otherwise.
+
+      number = self.to_s.split('').reverse.map { |x| x.to_i }
+      odd_words = (1..(number.size-1)).step(3).select {|x| x}
+      
+      position = 1
+      number.each do |x|
+         
+         
+         
+      
+      
       number = self
       trillions = number / 1_000_000_000_000
       number -= (trillions * 1_000_000_000_000)
@@ -22,67 +125,18 @@ class Fixnum
       tens = number / 10
       number -= (tens * 10)
       
-      case number
-      when 0
-         'zero'
-      when 1
-         'one'
-      when 2
-         'two'
-      when 3
-         'three'
-      when 4
-         'four'
-      when 5
-         'five'
-      when 6
-         'six'
-      when 7
-         'seven'
-      when 8
-         'eight'
-      when 9
-         'nine'
-      when 10
-         'ten'
-      when 11
-         'eleven'
-      when 12
-         'twelve'
-      when 13
-         'thirteen'
-      when 14
-         'fourteen'
-      when 15
-         'fifteen'
-      when 16
-         'sixteen'
-      when 17
-         'seventeen'
-      when 18
-         'eighteen'
-      when 19
-         'nineteen'
-      when 20
-         'twenty'
-      when 30
-         'thirty'
-      when 40
-         'forty'
-      when 50
-         'fifty'
-      when 60
-         'sixty'
-      when 70
-         'seventy'
-      when 80
-         'eighty'
-      when 90
-         'ninety'
-      when 100
-         'one hundred'
-      end
+      string = ''
+      
+      string += ugly_switch trillions
+      string += ugly_switch billions
+      string += ugly_switch millions
+      string += ugly_switch thousands
+      string += ugly_switch hundreds
+      string += ugly_switch tens
+      string += ugly_switch number
+      
+      string.chomp(" ")
+
    end
-   
    
 end
