a3dd96316301a32c85319c19fad6c450dd3c7305
Andrew <github@andrewmarrone.com>
2013-12-04 13:43:26 -0800

    Cleaner code

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -2,58 +2,16 @@ class Fixnum
    
    def in_words
 
-      return'zero' if self == 0    # special case, and breaks the pattern otherwise.
-      
-      ones = {
-               0 =>'',
-               1 =>'one',
-               2 =>'two',
-               3 =>'three',
-               4 =>'four',
-               5 =>'five',
-               6 =>'six',
-               7 =>'seven',
-               8 =>'eight',
-               9 =>'nine'
-      }
-      
-      teens = {
-               0 =>'ten',
-               1 =>'eleven',
-               2 =>'twelve',
-               3 =>'thirteen',
-               4 =>'fourteen',
-               5 =>'fifteen',
-               6 =>'sixteen',
-               7 =>'seventeen',
-               8 =>'eighteen',
-               9 =>'nineteen'
-      }
-      
-      tens = {
-               0 =>'',
-               1 =>'',
-               2 =>'twenty',
-               3 =>'thirty',
-               4 =>'forty',
-               5 =>'fifty',
-               6 =>'sixty',
-               7 =>'seventy',
-               8 =>'eighty',
-               9 =>'ninety'
-      }
-      
-      hundreds = {
-               1 =>'thousand',
-               2 =>'million',
-               3 =>'billion',
-               4 =>'trillion'
-               
-      }
-      
-      ones.default = ''
-      teens.default = ''
-      tens.default = ''
+      return'zero' if self == 0 # special case, and breaks the pattern otherwise.
+
+      ones = { 1 =>'one', 2 =>'two', 3 =>'three', 4 =>'four', 5 =>'five', 6 =>'six', 7 =>'seven', 8 =>'eight', 9 =>'nine' }
+      teens = { 0 =>'ten', 1 =>'eleven', 2 =>'twelve', 3 =>'thirteen', 4 =>'fourteen', 5 =>'fifteen', 6 =>'sixteen', 7 =>'seventeen', 8 =>'eighteen', 9 =>'nineteen' }
+      tens = { 2 =>'twenty', 3 =>'thirty', 4 =>'forty', 5 =>'fifty', 6 =>'sixty', 7 =>'seventy', 8 =>'eighty', 9 =>'ninety' }
+      hundreds = { 1 =>'thousand', 2 =>'million', 3 =>'billion', 4 =>'trillion' }
+
+      ones.default = '' 
+      teens.default = '' 
+      tens.default = '' 
       hundreds.default = ''
 
       number = self.to_s.split('').map { |x| x.to_i }
@@ -67,49 +25,28 @@ class Fixnum
          
          if set.size == 3
             string << ones[set[0]] << ( ones[set[0]] != '' ? 'hundred' : '' )
-            if set[1] == 1
-               string << teens[set[2]]
-            else
-               string << [tens[set[1]], ones[set[2]]]
-            end         
+            set[1] == 1 ? string << teens[set[2]] : string << [tens[set[1]], ones[set[2]]]
          elsif set.size == 2
-            if set[0] == 1
-               string << teens[set[1]]
-            else
-               string << [tens[set[0]], ones[set[1]]]
-            end            
+            set[0] == 1 ? string << teens[set[1]] :string << [tens[set[0]], ones[set[1]]]
          elsif set.size == 1
             string.unshift ones[set[0]]
          end
          
          string.flatten!
-         string.compact!
-
-         powers += 1 if number.empty? == false
-
-         # special case for single word events
-         
-#         if (number.empty? == false && self % 10 == 0)
-#            string.insert(0,hundreds[powers]) 
-#         elsif ! number.empty?
-#            string.unshift(hundreds[powers])
-#         end
-
          word.unshift string
          
       end
       
+      # inserts the 'special' words like hundreds, thousands, etc.
       result = []
       word.reverse!
       while ! word.empty?
          result << word.pop
-         result << ( result.last.uniq != [""] ? hundreds[word.size] : '' )
+         result << ( result.last.uniq != [''] ? hundreds[word.size] : '' )
       end
-            
-      #word = word.flatten.delete_if { |entry| entry == ''  }      
-      #word.delete('thousand') if self % 1_000_000 == 0
+      
+      # compact multidimensional arrays and delete '' [empty] entries
       result = result.flatten.delete_if { |entry| entry == ''  }
-      result
       result.join(' ')
       
    end
