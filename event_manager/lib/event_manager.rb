9cf576839a72c51aac1fbd4d567013d74b209daf
Andrew <github@andrewmarrone.com>
2013-12-12 15:51:36 -0800

    fixed date/time parsing

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -2,14 +2,17 @@ require 'csv'
 require 'sunlight/congress'
 require 'erb'
 require 'Date'
+require 'Time'
 
 Sunlight::Congress.api_key = 'e179a6973728c4dd3fb1204283aaccb5'
 
 def clean_zipcode zipcode
+
    zipcode.to_s.rjust(5, "0")[0..4]
 end
 
 def clean_phone number
+
    clean = "0"*10 
    
    case number.length
@@ -26,19 +29,25 @@ def clean_phone number
 end
 
 def time_target date
-   
-   d = Date.strptime(date, "%m/%d/%y %H:%M")
-   [date.strftime("%H"), d.strftime("%A")]
-   
+
+   day, hour = date.split
+   begin
+      day = (Date.strptime(day, '%m/%d/%y')).strftime '%A'
+      hour = (Time.parse hour).strftime '%H'
+   rescue
+      day, hour = 0, 0
+   end
+   [day, hour]
 end
 
 def legislators_by_zipcode zipcode
+
    legislators = Sunlight::Congress::Legislator.by_zipcode zipcode
 end
 
 def save_thank_you_letters id, form_letter
-   Dir.mkdir 'output' unless Dir.exists? 'output'
-   
+
+   Dir.mkdir 'output' unless Dir.exists? 'output'   
    filename = "output/thanks_#{id}.html"
    File.open filename, 'w' do |file|
       file.puts form_letter
@@ -51,21 +60,23 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 template_letter = File.read 'form_letter.erb'
 erb_template = ERB.new template_letter
 
-regdate = {}
+regday ||= {}
+regday.default = 0
 
 contents.each do |row|
+
    id = row[0]
    name = row[:first_name]
    zipcode = clean_zipcode row[:zipcode]   
    phone = clean_phone row[:homephone]
-   time = time_target row[:homephone]
+   day, hour = time_target row[:regdate]
    legislators = legislators_by_zipcode zipcode
    
-   regdate.map { regdate.haskey? time[0] ? regdate[time[0]].key += 1 : regdate[time[0]] = 1 }
+   regday[day] += 1
+   regday[hour] += 1
    
    form_letter = erb_template.result binding
 
 end
 
-
-puts regdate
+regday.sort.each { |key, value| puts "#{key} -> #{value}" }
